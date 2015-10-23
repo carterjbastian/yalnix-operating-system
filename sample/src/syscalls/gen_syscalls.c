3546db86d131216e73af4c21dc0798e273e103ee
konnorbeard <konnor.beard@gmail.com>
2015-10-22 21:35:51 -0400

    Little syscall work/load_program skeleton work

--- sample/src/syscalls/gen_syscalls.c
+++ sample/src/syscalls/gen_syscalls.c
@@ -1,17 +1,17 @@
 #include "kernel.h"
 
-int Fork() {
+int Yalnix_Fork() {
   // create_child_page_tables() 
   // create_child_pcb()
   // copy_kernal_stack_data_to_child()
 } 
 
-int Exec(char *filename, char **argvec) { 
+int Yalnix_Exec(char *filename, char **argvec) { 
   // filename.main(argvec[0], argvec.count[1]) ?
 }
 
 
-void Exit(int status) { 
+Void Yalnix_Exit(int status) { 
   // release_resources(GetPid());
   //   release locks cvars
   //   set children's parent to null
@@ -20,29 +20,42 @@ void Exit(int status) {
 } 
 
 
-int Wait(int *status_ptr) { 
+int Yalnix_Wait(int *status_ptr) { 
   // empty exited_children
   // switch to another available process
 
 } 
 
-int GetPid() { 
+int Yalnix_GetPid() { 
   return curr_proc->proc_id;
 } 
 
-int Brk(void *addr) {
+int Yalnix_Brk(void *addr) {
 
+  if ((unsigned int) addr > (unsigned int)KERNEL_STACK_BASE || addr < kernel_data_start) {
+    TracePrintf(1, "Brk Error: address requested not in bounds\n");
+    return ERROR;
+  }
+  
+  if (1) { // if we don't have enough memory 
+    TracePrintf(1, "Brk Error: not enough memory available");
+    return ERROR;
+  } 
+
+  
+  
+  
 }
 
-int Delay(int clock_ticks) { 
-  // if (clock_ticks < 0) return ERROR
-  // else if (clock_ticks == 0) return 0;
-  // else 
-  //   curr_proc->delay_clock_ticks = clock_ticks
-  //   return 0 when curr_proc->delay_clock_ticks == 0
+int Yalnix_Delay(int clock_ticks) { 
+  if (clock_ticks < 0) return ERROR;
+  if (clock_ticks == 0) return 0;
+  clock_t t = clock();
+  while(t - clock_ticks > 0){};
+  return;
 } 
 
-int Reclaim(int id) { 
+int Yalnix_Reclaim(int id) { 
   // for lock/cvar/pipe in locks + cvars + pipes
   //   destory_and_release(item with id == id)
 } 
